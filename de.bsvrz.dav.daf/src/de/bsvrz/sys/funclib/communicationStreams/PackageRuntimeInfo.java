97bf8ca4c54b7e03a0be94a6c57630c21314647c
upeuker <dev@upeuker.net>
2014-03-14 14:17:59 +0100

    Klassen aus zirkulären Abhängigkeiten der Original-Software wiederhergestellt

---
+++ de.bsvrz.dav.daf/src/de/bsvrz/sys/funclib/communicationStreams/PackageRuntimeInfo.java
@@ -0,0 +1,14 @@
+
+            package de.bsvrz.sys.funclib.communicationStreams;
+            public class PackageRuntimeInfo {
+            public static String getLicence() { return "LGPL";}
+            public static String getRelease() { return "Kernsoftware 3.6.1";}
+            public static String getVersion() { return "3.6.1.v12117";}
+            public static String getRevision() { return "12117";}
+            public static String getCompileTime() { return "13.03.2014 20:31:44";}
+            public static String getDependsOnCompiled() { return "de.bsvrz.sys.funclib.concurrent, de.bsvrz.sys.funclib.dataSerializer, de.bsvrz.sys.funclib.debug, de.bsvrz.dav.daf";}
+            public static String getDependsOnSource() { return "";}
+            public static String getDependsOnLib() { return "";}
+            public static String getJvmVersion() { return "1.6";}
+            }
+        
---
+++ de.bsvrz.dav.daf/src/de/bsvrz/sys/funclib/dataSerializer/PackageRuntimeInfo.java
@@ -0,0 +1,14 @@
+
+            package de.bsvrz.sys.funclib.dataSerializer;
+            public class PackageRuntimeInfo {
+            public static String getLicence() { return "LGPL";}
+            public static String getRelease() { return "Kernsoftware 3.6.1";}
+            public static String getVersion() { return "3.6.1.v12117";}
+            public static String getRevision() { return "12117";}
+            public static String getCompileTime() { return "13.03.2014 20:31:36";}
+            public static String getDependsOnCompiled() { return "de.bsvrz.dav.daf, de.bsvrz.sys.funclib.debug";}
+            public static String getDependsOnSource() { return "";}
+            public static String getDependsOnLib() { return "";}
+            public static String getJvmVersion() { return "1.6";}
+            }
+        
